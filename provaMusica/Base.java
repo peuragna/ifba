bab1c5cef4272ec1fd16262d27ae24fb91df4887
Pedro <57912295+peuragna@users.noreply.github.com>
2019-11-22 21:46:58 -0300

    Add files via upload

---
+++ provaMusica/Base.java
@@ -0,0 +1,15 @@
+public class Base{
+    Playlist[] playlists;
+    
+    public Base(int tamanho){
+        this.playlists = new Playlist[tamanho];
+    };
+    
+    public Base(){
+        this(1);
+    };
+    
+  
+ 
+    
+}
--- provaMusica/Playlist.java
+++ provaMusica/Playlist.java
@@ -9,6 +9,10 @@ public class Playlist{
         this(1);
     };
     
+    public Musica getMusica(int indice){
+        return this.musicas[indice];
+    };
+    
     public Musica[] getLista(){
         return this.musicas;
     };
@@ -23,5 +27,33 @@ public class Playlist{
         this.musicas = aux;
     };
     
+    public Playlist gerarPlaylist(Playlist outra, String nome, double duraçao){
+        Playlist[] novaPlaylist = this(1); //NAO É ARRAY DE PLAYLIST
+        double[] proximidades = new double[this.musicas.length];
+        double maior = 0;
+        int indice = 0;
+        
+        for(int i = 0; i < this.musicas.length; i++)
+            for(Musica outraMusica : outra.musicas)
+                proximidades[i] = this.musicas[i].calcularProximidade(outraMusica);
+               
+        for(int i = 0; i < proximidades.length; i++)
+            if(proximidades[i] > maior){
+                maior = proximidades[i];
+                indice = i;
+            }
+            
+        if(duraçao > 0){
+            novaPlaylist.addMusica(outra.getMusica(indice));
+            
+            
+            
+        }
+        
+            
+        
+    };
+    
+   
     
 }
