f5b94fed8c603bc12681cdc060c5ce10d6e9cfba
Pedro <57912295+peuragna@users.noreply.github.com>
2019-11-26 20:13:42 -0300

    Add files via upload

--- provaAtomo/Molecula.java
+++ provaAtomo/Molecula.java
@@ -11,4 +11,28 @@ public class Molecula{
         return menor;         
     };
     
+    public int getNumAtomos(Atomo atomo){
+        int qtd = 0;
+        
+        for(Atomo este : this.atomos)
+            if(atomo.equals(este))
+                qtd++;
+        
+        return qtd;
+    };
+    
+    public boolean isPresente(Molecula outra){
+        for(Atomo outros : outra.atomos)
+            if(this.getNumAtomos(outros) == 0)
+                return false;
+        
+        return true;
+    };
+    
+     public boolean isComposto(Molecula outra){
+         if(this.isPresente(outra) == true && outra.isPresente(this) == true)
+            return true;
+         
+         return false;
+    };
 }
